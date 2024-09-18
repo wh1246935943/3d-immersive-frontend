0b2ca9fd640d02bb141272f4b4939581f3b544d3
wh1246935943 <1246935943@qq.com>
2024-09-19 03:14:28 +0800

    动态加载全景图像

--- src/components/SphereViewer/index.tsx
+++ src/components/SphereViewer/index.tsx
@@ -3,13 +3,19 @@
 import SphereViewer, { type Viewer } from '@/components/SphereViewer/SphereViewer';
 
 import { AutorotatePlugin } from '@photo-sphere-viewer/autorotate-plugin';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import './style.less';
 
-import img1 from '@/assets/1.jpg';
+// import img1 from '@/assets/1.jpg';
 
-import img2 from '@/assets/2.jpg';
+// import img2 from '@/assets/2.jpg';
+
+const imgList = [
+  new URL(`@/assets/1.jpg`, import.meta.url).href,
+  new URL(`@/assets/2.jpg`, import.meta.url).href,
+  new URL(`@/assets/3.jpg`, import.meta.url).href
+];
 
 function SphereViewerBox() {
 
@@ -17,11 +23,13 @@ function SphereViewerBox() {
   const viewerInsRef = useRef<Viewer | null>(null);
   const autorotateRef = useRef<AutorotatePlugin | null>(null);
 
+  const [ imageList ] = useState<string[]>(imgList);
+
   useEffect(() => {
 
     sphereViewerRef.current = new SphereViewer({
       container: '#viewerContainer',
-      panorama: img2,
+      panorama: imageList[0],
       defaultZoomLvl: 30,
       navbar: false,
       plugins: [
@@ -54,10 +62,19 @@ function SphereViewerBox() {
   return (
     <>
       <div id="viewerContainer"></div>
-      <div className="option absolute bottom-20 left-10 flex">
-        <button className="bg-blue-500 text-slate-50 mr-3 p-2" onClick={setPosition}>停止/开始旋转</button>
-        <img onClick={switchPanorama.bind(null, img1)} src={img1} className="w-20 h-10 mr-3" alt="" />
-        <img onClick={switchPanorama.bind(null, img2)} src={img2} className="w-20 h-10" alt="" />
+      <div className="options absolute bottom-5 left-5 flex flex-col">
+        <div className="">
+          <button className="bg-blue-500 text-slate-50 mr-3 p-2" onClick={setPosition}>停止/开始旋转</button>
+        </div>
+        <div className="flex mr-3 mt-3">
+          {
+            imageList.map((item, index) => {
+              return (
+                <img key={index} onClick={switchPanorama.bind(null, item)} src={item} className="w-20 h-10 mr-3" alt="" />
+              )
+            })
+          }
+        </div>
       </div>
     </>
   )
