a0a9a504c3abd9376a89a553aefddb5ea5b20ba1
wh1246935943 <1246935943@qq.com>
2024-09-19 16:43:20 +0800

    配置生产和开发环境参数，添加页面标识内容

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,14 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname,'./src')
-    }
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'development' ? '/' : '/case/3d-immersive/',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname,'./src')
+      }
+    },
+  }
 })
